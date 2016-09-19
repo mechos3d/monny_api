e87751ba367b773ab3f419a9de63f3983e70e526
gorg <gorgolevsky@gmail.com>
2016-09-19 23:40:10 +0300

    added most basic functionality to create and show records

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,25 @@
 class WelcomeController < ApplicationController
   def index
-    render json: { hello: 'world' }
+    @topics = Topic.all
+    render json: @topics
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+    render json: @topic
+  end
+
+  def create
+    if topic = Topic.create(topic_params)
+      render json: { success: topic.id }
+    else
+      render json: topic.errors
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:string_field1, :int_field1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  get '/topics/:id', to: 'welcome#show'
+  post '/topics', to: 'welcome#create'
+
 end
