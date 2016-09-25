074b9c43ef6bb1cbccfc87f19a2f1deca1b7e2c0
gorg <gorgolevsky@gmail.com>
2016-09-25 22:46:47 +0300

    small_additions in syncs controller

--- app/controllers/syncs_controller.rb
+++ app/controllers/syncs_controller.rb
@@ -1,12 +1,13 @@
 class SyncsController < ApplicationController
 
   def index # currently only for testing-debugging
-    Record.all
+    @records = Record.all
+    render json: @records.to_json
   end
 
   def create
     # TODO: make it in transaction
-    records = sync_params[:records].map do |record_params|
+    @records = sync_params[:records].map do |record_params|
       Record.create(record_params)
     end
     # TODO: make it return errors (especially after adding validations
