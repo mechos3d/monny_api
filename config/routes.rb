77ac7f78337a598e98575f86ac790faf3a51f78a
Gorgolevskiy Pavel <p.gorgolevskiy@rambler-co.ru>
2017-08-18 13:08:03 +0300

    removed v1/syncs route and controller completely - now access is only with auth_token

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resource :presentation, only: [:show]
-  resources :syncs, only: %i[index create]
-
   namespace 'v2' do
     resources :syncs, only: %i[index create]
   end
