91a00118d3e4acac2a1a3a0800211bc5555037cc
Gorgolevskiy Pavel <gorgolevsky@gmail.com>
2021-02-28 00:07:49 +0300

    Added a comment with example of curl for backup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   resource :presentation, only: [:show]
   namespace 'v2' do
+
+    # NOTE: for backup:
+    # curl --header 'Authorization: Bearer <token>' 'https://<host>/v2/syncs?backup=true'
     resources :syncs, only: %i[index create]
   end
 end
